e616a2ed893f5771cdba75336b243cbee3b35cb3
levelopers <youcancallmeallen@gmail.com>
2019-04-03 21:40:46 -0400

    audit fix 3/Apr/2019

--- express_form/examforms.js
+++ express_form/examforms.js
@@ -1,23 +1,11 @@
-// examforms.js
-// Anil Somayaji
-// February 1, 2016
-// For Carleton University, COMP 2406 2016W
-//
-// To install dependencies, run:
-//    npm install express body-parser morgan jade
-//
-
 var http = require('http');
 var express = require('express');
 var bodyParser = require('body-parser');
 var logger = require('morgan');
 const fs = require('fs')
 // process.argv.push(process.argv.shift())
-var port = process.argv[2]||3000;
+var port = process.argv[2] || 3000;
 var state = [];
-
-
-
 
 var app = express();
 
@@ -27,49 +15,49 @@ app.use(logger('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('.'));//set dirctory to serve file
 
-//app.get can only render html page 
-//but can not serve files like scripts 
-app.get('/', function(req, res, next) {
-  res.render('index', { title: 'COMP 2406 Exam form demo' });
+app.get('/', function (req, res, next) {
+    res.render('index', { title: 'Welcome to form demo' });
 });
 
-app.post('/add', function(req, res) {
-    var obj = { name: req.body.name,
-		city:   req.body.city,
+app.post('/add', function (req, res) {
+    var obj = {
+        name: req.body.name,
+        city: req.body.city,
         country: req.body.country,
         birthday: req.body.birthday,
         email: req.body.email,
-        phoneNumber: req.body.phone };
+        phoneNumber: req.body.phone
+    };
     state.push(obj);
     res.redirect('/list');
 });
 
 //delete a name
-app.post('/delete',function(req,res){
-     const newState = state.filter(e=>{return e.name!==req.body.nameDelete})
+app.post('/delete', function (req, res) {
+    const newState = state.filter(e => e.name !== req.body.nameDelete)
     state = newState
-    res.render('delete',{title: 'deleted a name', items: state})
+    res.render('delete', { title: 'deleted a name', items: state })
 })
 
-
-app.get('/list', function(req, res) {
-    res.render('list', { title: 'People Listing',  items: state});
+app.get('/list', function (req, res) {
+    res.render('list', { title: 'People Listing', items: state });
 });
 
-var serverUp = function() {
-    console.log("ExamForms listening on port " + port);
-    fs.readFile('prevState.json',(err,data)=>{
-        if(err) throw err
+//read prevState.json file
+var serverUp = function () {
+    console.log("listening on port " + port);
+    fs.readFile('prevState.json', (err, data) => {
+        if (err) throw err
         let dataObj = JSON.parse(data)
         state = dataObj
     })
-    // process.argv.forEach(e=>{console.log(e)})
 }
 
-var serverDown = function() {
+//save file
+var serverDown = function () {
     console.log("Server shutting down.");
-    fs.writeFile('prevState.json',JSON.stringify(state),'utf8',err=>{
-        if(err) throw err
+    fs.writeFile('prevState.json', JSON.stringify(state), 'utf8', err => {
+        if (err) throw err
         process.exit(0)
     })
 }
